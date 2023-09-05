b8632aa6380186502e6baa45df6aa77644f53d22
Ukamaka Okpala <111020223+Ukokpala@users.noreply.github.com>
2023-09-05 22:08:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import controllerRouting from './routes/index';
-
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
+// Middleware to parse JSON requests
 app.use(express.json());
 
-controllerRouting(app);
+// Load routes from routes/index.js
+const routes = require('./routes');
+app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-export default app;
