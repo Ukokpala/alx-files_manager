73b1f7f674ce3fdcc8ad94f91d10c022e4bb66f5
Ukamaka Okpala <111020223+Ukokpala@users.noreply.github.com>
2023-09-06 01:06:05 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,18 @@
+#!/usr/bin/node
+
 const express = require('express');
-const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController'); // Import the FilesController
 
+const router = express.Router();
 
-// GET /status => AppController.getStatus
 router.get('/status', AppController.getStatus);
-
-// GET /stats => AppController.getStats
 router.get('/stats', AppController.getStats);
-
-// POST /users => UsersController.postNew
 router.post('/users', UsersController.postNew);
-
-// GET /connect => AuthController.getConnect
 router.get('/connect', AuthController.getConnect);
-
-// GET /disconnect => AuthController.getDisconnect
 router.get('/disconnect', AuthController.getDisconnect);
-
-// GET /users/me => UsersController.getMe
-router.get('/users/me', UsersController.getMe);
-
-// POST /files => FilesController.postUpload
-router.post('/files', FilesController.postUpload);
-
-// GET /files/:id => FilesController.getShow
-router.get('/files/:id', FilesController.getShow);
-
-// GET /files => FilesController.getIndex
-router.get('/files', FilesController.getIndex);
-
-// PUT /files/:id/publish => FilesController.putPublish
-router.put('/files/:id/publish', FilesController.putPublish);
-
-// PUT /files/:id/unpublish => FilesController.putUnpublish
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/users/me', AuthController.getMe);
 
 module.exports = router;
 
